ae234b4d04d3d648beec9c141926725a28f8d5ae
Emil Karimov <emvakar@gmail.com>
2022-10-25 01:32:07 +0530

    update

--- HandToHand/SceneDelegate.swift
+++ HandToHand/SceneDelegate.swift
@@ -13,11 +13,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        configureWindow(with: scene)
     }
+
+}
+
+// MARK: - Private
+
+private extension SceneDelegate {
+
+    func configureWindow(with scene: UIScene) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let rootController = RootViewController(resolver: resolver)
+        window.rootViewController = rootController
+        self.window = window
+    }
+}
+
+extension SceneDelegate {
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
@@ -47,6 +61,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
